5dd0380ca69f5a305a1e0d329489f3db3f150035
Josh Manderson <josh.manderson@live.com.au>
2018-07-06 14:28:08 +1000

    Removed 'static' usage

--- Grid.js
+++ Grid.js
@@ -1,38 +1,38 @@
-import React from 'react';
-import { oneOfType, arrayOf, string, number } from 'prop-types';
-import styled from 'styled-components';
+import React from "react";
+import { oneOfType, arrayOf, string, number } from "prop-types";
+import styled from "styled-components";
 
-import { generateDefaultStyle, generateMediaQueries } from './utils';
+import { generateDefaultStyle, generateMediaQueries } from "./utils";
 
 const generateGridTemplateColumnsOrRows = props => {
   const { num, dimension, fixed } = props;
 
   if (num) {
-    return `repeat(${num}, ${dimension || '1fr'})`;
+    return `repeat(${num}, ${dimension || "1fr"})`;
   } else if (fixed) {
     return fixed;
   } else {
-    return '';
+    return "";
   }
 };
 
 const generateStyle = props => `
   display: grid;
-  align-items: ${props.alignItems || ''};
-  justify-items: ${props.justifyItems || ''};
-  grid-gap: ${props.gridGap || ''};
-  grid-auto-rows: ${props.rowHeight || ''};
+  align-items: ${props.alignItems || ""};
+  justify-items: ${props.justifyItems || ""};
+  grid-gap: ${props.gridGap || ""};
+  grid-auto-rows: ${props.rowHeight || ""};
   grid-template-rows: ${generateGridTemplateColumnsOrRows({
     num: props.numRows,
     dimension: props.rowHeight,
-    fixed: props.fixedRows,
-  }) || ''};
-  grid-auto-columns: ${props.columnWidth || ''};
+    fixed: props.fixedRows
+  }) || ""};
+  grid-auto-columns: ${props.columnWidth || ""};
   grid-template-columns: ${generateGridTemplateColumnsOrRows({
     num: props.numColumns,
     dimension: props.columnWidth,
-    fixed: props.fixedColumns,
-  }) || ''};
+    fixed: props.fixedColumns
+  }) || ""};
 `;
 
 const GridWrapper = styled.div`
@@ -41,30 +41,9 @@ const GridWrapper = styled.div`
 `;
 
 class Grid extends React.Component {
-  static propTypes = {
-    breakpoints: arrayOf(string),
-    alignItems: oneOfType([string, arrayOf(string)]),
-    justifyItems: oneOfType([string, arrayOf(string)]),
-    gridGap: oneOfType([string, arrayOf(string)]),
-    numRows: oneOfType([number, arrayOf(number)]),
-    rowHeight: oneOfType([string, arrayOf(string)]),
-    fixedRows: oneOfType([string, arrayOf(string)]),
-    numColumns: oneOfType([number, arrayOf(number)]),
-    columnWidth: oneOfType([string, arrayOf(string)]),
-    fixedColumns: oneOfType([string, arrayOf(string)]),
-  };
-
-  static defaultProps = {
-    breakpoints: ['40em', '52em', '64em'],
-  };
-
-  static childContextTypes = {
-    breakpoints: arrayOf(string),
-  };
-
   getChildContext() {
     return {
-      breakpoints: this.props.breakpoints,
+      breakpoints: this.props.breakpoints
     };
   }
 
@@ -72,5 +51,26 @@ class Grid extends React.Component {
     return <GridWrapper {...this.props} />;
   }
 }
+
+Grid.propTypes = {
+  breakpoints: arrayOf(string),
+  alignItems: oneOfType([string, arrayOf(string)]),
+  justifyItems: oneOfType([string, arrayOf(string)]),
+  gridGap: oneOfType([string, arrayOf(string)]),
+  numRows: oneOfType([number, arrayOf(number)]),
+  rowHeight: oneOfType([string, arrayOf(string)]),
+  fixedRows: oneOfType([string, arrayOf(string)]),
+  numColumns: oneOfType([number, arrayOf(number)]),
+  columnWidth: oneOfType([string, arrayOf(string)]),
+  fixedColumns: oneOfType([string, arrayOf(string)])
+};
+
+Grid.defaultProps = {
+  breakpoints: ["40em", "52em", "64em"]
+};
+
+Grid.childContextTypes = {
+  breakpoints: arrayOf(string)
+};
 
 export default Grid;
--- GridItem.js
+++ GridItem.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import { oneOfType, arrayOf, string, number } from 'prop-types';
-import styled from 'styled-components';
+import React from "react";
+import { oneOfType, arrayOf, string, number } from "prop-types";
+import styled from "styled-components";
 
-import { generateDefaultStyle, generateMediaQueries } from './utils';
+import { generateDefaultStyle, generateMediaQueries } from "./utils";
 
 const generateStyle = props => `
-  grid-area: ${props.gridArea || ''};
-  grid-row-start: ${props.rowStart || ''};
+  grid-area: ${props.gridArea || ""};
+  grid-row-start: ${props.rowStart || ""};
   grid-row-end: ${
-    props.rowEnd ? props.rowEnd : props.rowSpan ? `span ${props.rowSpan}` : ''
+    props.rowEnd ? props.rowEnd : props.rowSpan ? `span ${props.rowSpan}` : ""
   };
-  grid-column-start: ${props.columnStart || ''};
+  grid-column-start: ${props.columnStart || ""};
   grid-column-end: ${
     props.columnEnd
       ? props.columnEnd
       : props.columnSpan
         ? `span ${props.columnSpan}`
-        : ''
+        : ""
   };
 `;
 
@@ -26,25 +26,25 @@ const GridItemWrapper = styled.div`
 `;
 
 class GridItem extends React.Component {
-  static propTypes = {
-    rowStart: oneOfType([number, arrayOf(number)]),
-    rowEnd: oneOfType([number, arrayOf(number)]),
-    rowSpan: oneOfType([number, arrayOf(number)]),
-    columnStart: oneOfType([number, arrayOf(number)]),
-    columnEnd: oneOfType([number, arrayOf(number)]),
-    columnSpan: oneOfType([number, arrayOf(number)]),
-    gridArea: oneOfType([string, arrayOf(string)]),
-  };
-
-  static contextTypes = {
-    breakpoints: arrayOf(string),
-  };
-
   render() {
     return (
       <GridItemWrapper breakpoints={this.context.breakpoints} {...this.props} />
     );
   }
 }
+
+GridItem.propTypes = {
+  rowStart: oneOfType([number, arrayOf(number)]),
+  rowEnd: oneOfType([number, arrayOf(number)]),
+  rowSpan: oneOfType([number, arrayOf(number)]),
+  columnStart: oneOfType([number, arrayOf(number)]),
+  columnEnd: oneOfType([number, arrayOf(number)]),
+  columnSpan: oneOfType([number, arrayOf(number)]),
+  gridArea: oneOfType([string, arrayOf(string)])
+};
+
+GridItem.contextTypes = {
+  breakpoints: arrayOf(string)
+};
 
 export default GridItem;
